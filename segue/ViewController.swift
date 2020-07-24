7a6386a9747d181ab927cb5c75f5aec2ebb8372d
Baaro <show0007@msn.com>
2020-07-24 16:37:16 +0800

    Initial Commit

---
+++ segue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  segue
+//
+//  Created by 林家宇 on 2020/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
